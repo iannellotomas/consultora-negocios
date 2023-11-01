d7219cf26583ad3b98a7ebb5e86123d7c8f31c74
Tomás Iannello <106415807+iannellotomas@users.noreply.github.com>
2023-11-01 13:18:49 -0300

    Delete src directory

--- src/components/subtitleSection/SubTitleSection.jsx
+++
@@ -1,5 +0,0 @@
-import styles from "./SubTitleSection.module.css";
-
-export default function SubTitleSection({ content }) {
-  return <h2 className={styles.subTitleSection}>{content}<span>.</span></h2>;
-}
